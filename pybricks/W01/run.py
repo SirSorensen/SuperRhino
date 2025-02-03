2879d0b8a6ee307d3d841aa8aea590a40061f6d9
Mahmood Mohammed Seoud <mse@peoplenet.dk>
2025-02-03 09:24:08 +0100

    init

--- pybricks/W01/run.py
+++ pybricks/W01/run.py
@@ -16,18 +16,20 @@ left_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
 
 # Initialize the drive base. In this example, the wheel diameter is 56mm.
 # The distance between the two wheel-ground contact points is 112mm.
-drive_base = DriveBase(left_motor, right_motor, wheel_diameter=56, axle_track=80)
+drive_base = DriveBase(left_motor, right_motor,
+                       wheel_diameter=56, axle_track=80)
+drive_base.settings(turn_rate=20)
+
+drive_base.turn(20)
 
 
-drive_base.straight(900)
+# drive_base.straight(900)
 
-# turn left
-drive_base.turn(-30)
+# turn left testing
 
-drive_base.straight(450)
+# drive_base.straight(450)
 
-# The robot starts on the floor and 
-# can be commanded to move forward 90cm and 
-# left 45cm and point at an angle relatively 
+# The robot starts on the floor and
+# can be commanded to move forward 90cm and
+# left 45cm and point at an angle relatively
 # to the starting direction of 30 degrees
-
