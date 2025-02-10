16ebfbd377709d5afde0883099ef29b498c3153f
Mahmood Mohammed Seoud <mse@peoplenet.dk>
2025-02-10 09:46:26 +0100

    pyplot relfections

---
+++ pybricks/W03/plots/reflection.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+
+distances = [
+    1.5,
+    3,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    20,
+    30
+]
+reflections = [
+    34,
+    13,
+    7,
+    4,
+    3,
+    2,
+    2,
+    1,
+    1,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0
+]
+
+plt.plot(distances, reflections)
+plt.xlabel('distances')
+plt.ylabel('reflections')
+plt.show()
