198c6c12ca5d5a7e6cae5fbf9bcbb6f6b571b930
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2025-03-17 15:34:31 +0100

    Refactor : Fixed types

--- pybricks/senses/vision.py
+++ pybricks/senses/vision.py
@@ -14,27 +14,25 @@ class Vision:
 
         self.calibrate()
 
-
-
     ########################## Calibrations: ##########################
 
-    def calibrate(self) -> float:
-        def min_max():
+    def calibrate(self):
+        def min_max() -> tuple[float, float]:
             (min_left, max_left) = cal.min_max_measure(self.left_sensor.reflection)
             (min_right, max_right) = cal.min_max_measure(self.right_sensor.reflection)
             return (min(min_left, min_right), max(max_left, max_right))
 
         print("Please put my eyes over table, and press Enter.")
         input()
-        self.table_range = min_max()
+        self.table_range : tuple[float, float] = min_max()
 
         print("Please put my eyes over tape, and press Enter.")
         input()
-        self.tape_range = min_max()
+        self.tape_range : tuple[float, float] = min_max()
 
         print("Please put my eyes over an edge, and press Enter.")
         input()
-        self.edge_range = min_max()
+        self.edge_range : tuple[float, float] = min_max()
 
         if self.edge_range[1] >= self.tape_range[0]:
             print(f"ERROR! edge_range and tape_range overlap! edge_range:{self.edge_range} tape_range:{self.tape_range}")
