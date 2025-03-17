3c65427e17e98e7cb250a2a8a95bb1cc7514594f
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2025-03-17 09:59:50 +0100

    Refactor : Unused eye removal

--- pybricks/robot.py
+++ pybricks/robot.py
@@ -1,22 +1,16 @@
-from senses.vision import Vision
 from physiology.movement import Movement
 from pybricks.parameters import Port
 from pybricks.hubs import PrimeHub
-from pybricks.tools import wait
 
 
 class Robot:
     def __init__(self):
-        ### Measurements ###
-        # About 8 cm from eye to leg
 
         # Initialise Motors (wheels)
         self.legs: Movement = Movement(Port.B, Port.A)
 
-        # Initialize & calibrate the sensors
-        # We currently have no eyes
-        # self.eyes : RobotEyes = RobotEyes(Port.B, Port.F)
-
         # Initialise PrimeHub
         self.prime_hub: PrimeHub = PrimeHub()
         self.prime_hub.speaker.volume(50)
+
+
