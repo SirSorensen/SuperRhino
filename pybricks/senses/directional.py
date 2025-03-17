69384cf4f9b49934816576dd7e59b84f5d6a086d
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2025-03-17 13:55:34 +0100

    Feat : More Sense_of_Direction

--- pybricks/senses/directional.py
+++ pybricks/senses/directional.py
@@ -12,6 +12,7 @@ class Sense_of_Direction:
 
         # Calibrations:
         self.calibrate_acceleration()
+        self.reset()
         self.heading_errors = []
         self.heading_threshold = 0
 
@@ -19,6 +20,25 @@ class Sense_of_Direction:
     def direction(self) -> float:
         """Return a float between -179 and 180 which is based on calibrated threshold."""
         return Angle.to_angle(self.imu.heading() - self.heading_threshold)
+
+    def reset(self):
+        self.imu.reset_heading(0)
+
+
+    def is_direction_correct(self, correct_direction):
+        error = Angle.calc_error(correct_direction, self.imu.heading())
+
+        if Angle.in_diff_domains(self.heading_threshold, error):
+            return False
+
+        return self.heading_threshold >= error
+
+    def calc_direction_error(self, correct_direction):
+        if self.is_direction_correct(correct_direction):
+            return 0
+        else:
+            return Angle.calc_error(correct_direction, self.direction())
+
 
 
     ########################## Calibrations: ##########################
@@ -31,21 +51,13 @@ class Sense_of_Direction:
         self.acceleration_error = {"X": avg_acc_x_stationary, "Y": avg_acc_y_stationary, "Z": acceleration_error_z}
 
     def add_heading_error(self, current_degree) -> float:
-        measure_angle = Angle.to_angle(avg_measure(self.imu.heading))
+        measured_angle = Angle.to_angle(avg_measure(self.imu.heading))
         current_angle = Angle.to_angle(current_degree)
         # If i.e. current_angle = 179 and measure_angle = -179
-        if (current_angle >= 0 > measure_angle or measure_angle >= 0 > current_angle) and (abs(current_angle) > 100 and abs(measure_angle) > 100):
-            print(f"({current_angle} >= 0 > {measure_angle} or {measure_angle} >= 0 > {current_angle}) = True" )
-            if current_angle >= 0 > measure_angle:
-                error = current_degree - (measure_angle + 360)
-            else:
-                error = current_degree - (measure_angle - 360)
-        else:
-            error = current_degree - measure_angle
-
+        error = Angle.calc_error(current_angle, measured_angle)
         self.heading_errors.append(error)
 
     def finish_calibrate_heading(self):
         avg_heading_error = sum(self.heading_errors) / len(self.heading_errors)
         max_heading_error = max(self.heading_errors)
-        self.heading_threshold = (avg_heading_error + max_heading_error) / 2
+        self.heading_threshold = Angle.to_angle((avg_heading_error + max_heading_error) / 2)
--- pybricks/utils/angle.py
+++ pybricks/utils/angle.py
@@ -10,5 +10,20 @@ class Angle:
         angle = Angle(degree)
         return angle.value
 
+    def in_diff_domains(degree_1 : float, degree_2 : float):
+        angle_1 = Angle.to_angle(degree_1)
+        angle_2 = Angle.to_angle(degree_2)
+        return (angle_1 >= 0 > angle_2) or (angle_2 >= 0 > angle_1)
 
-
+    def calc_error(current_degree : float, measured_degree : float):
+        current_angle = Angle.to_angle(current_degree)
+        measured_angle = Angle.to_angle(measured_degree)
+        # If i.e. current_angle = 179 and measure_angle = -179
+        if Angle.in_diff_domains(current_angle, measured_angle) and (abs(current_angle) > 100 and abs(measured_angle) > 100):
+            print(f"({current_angle} >= 0 > {measured_angle} or {measured_angle} >= 0 > {current_angle}) = True" )
+            if current_angle >= 0 > measured_angle:
+                return current_angle - (measured_angle + 360)
+            else:
+                return current_angle - (measured_angle - 360)
+        else:
+            return current_angle - measured_angle
