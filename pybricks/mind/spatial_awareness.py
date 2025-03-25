bfe6cd2861b286108917f33d8dd10fee4361445a
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2025-03-25 14:49:49 +0100

    Feat : Eye point calculations implemented

--- pybricks/mind/spatial_awareness.py
+++ pybricks/mind/spatial_awareness.py
@@ -4,9 +4,14 @@ from mind.utils.trigonometry import Trigonometry
 
 
 class Spatial_Awareness:
-    def __init__(self, start_position: tuple[float, float] = (0, 0), start_direction: CardinalDirection = CardinalDirection.E):
+    def __init__(self, dist_to_eye: tuple[float, float], start_position: tuple[float, float] = (0, 0), start_direction: CardinalDirection = CardinalDirection.E):
         self.cur_direction: CardinalDirection = start_direction
         self.cur_position: Point = Point(start_position)
+
+
+        eye_x, eye_y = dist_to_eye
+        self.dist_eye_right = Point((abs(eye_x), eye_y))
+        self.dist_eye_left = Point((-abs(eye_x), eye_y))
 
         self.last_dist = 0
 
@@ -22,3 +27,8 @@ class Spatial_Awareness:
         # Update properties
         self.cur_position.update(change_vector)
         self.last_dist = dist
+
+    def get_eyes_posses(self) -> tuple[Point, Point]:
+        left = self.cur_position.sum(self.dist_eye_left)
+        right = self.cur_position.sum(self.dist_eye_right)
+        return (left, right)
--- pybricks/mind/utils/point.py
+++ pybricks/mind/utils/point.py
@@ -11,3 +11,8 @@ class Point(object):
         if type(other) is not Point:
             raise KeyError("other has to be a Point")
         return (other.X - self.X, other.Y - self.Y)
+
+    def sum(self, other):
+        if type(other) is not Point:
+            raise KeyError("other has to be a Point")
+        return Point(other.X + self.X, other.Y + self.Y)
