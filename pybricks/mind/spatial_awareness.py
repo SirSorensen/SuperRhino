2550b5272f0e55c5f69304abd12e79e948647c06
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2025-03-25 14:54:07 +0100

    Style : Format

--- pybricks/mind/spatial_awareness.py
+++ pybricks/mind/spatial_awareness.py
@@ -8,17 +8,18 @@ class Spatial_Awareness:
         self.cur_direction: CardinalDirection = start_direction
         self.cur_position: Point = Point(start_position)
 
-
         eye_x, eye_y = dist_to_eye
         self.dist_eye_right = Point((abs(eye_x), eye_y))
         self.dist_eye_left = Point((-abs(eye_x), eye_y))
 
         self.last_dist = 0
 
+
     def next_angle(self, next_dir: CardinalDirection) -> float:
         result = diff(self.cur_direction, next_dir)
         self.cur_direction = next_dir
         return result
+
 
     def update_pos(self, dist, cur_heading):
         # Calcs
@@ -27,6 +28,7 @@ class Spatial_Awareness:
         # Update properties
         self.cur_position.update(change_vector)
         self.last_dist = dist
+
 
     def get_eyes_posses(self) -> tuple[Point, Point]:
         left = self.cur_position.sum(self.dist_eye_left)
