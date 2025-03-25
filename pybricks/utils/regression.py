749bf38dc054a9b8706eaebd238dfdbab42806a8
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2025-03-25 16:59:00 +0100

    Refactor : Moved calc_angle to Trigonometry

--- pybricks/utils/regression.py
+++ pybricks/utils/regression.py
@@ -3,23 +3,6 @@ import umath as math
 
 
 class Regression:
-    def calc_angle(point1: Point, point2: Point):
-        # cos(a) = abs(x) / sqrt(x^2+y^2)
-        vx, vy = point1.to_vector(point2)
-        v_length = math.sqrt(math.pow(vx, 2) + math.pow(vy, 2))
-        x_length = abs(vx)
-        abs_radians = math.acos(x_length / v_length)
-        abs_degrees = math.degrees(abs_radians)
-
-        if vx < 0 and vy < 0:
-            abs_degrees += 180
-        elif vx < 0:
-            abs_degrees += 90
-        elif vy < 0:
-            abs_degrees += 270
-
-        return abs_degrees
-
     def linear_regression(point1: Point, point2: Point):
         vx, vy = point1.to_vector(point2)
         slope = vy / vx
--- pybricks/utils/trigonometry.py
+++ pybricks/utils/trigonometry.py
@@ -7,8 +7,27 @@ class Trigonometry:
         y = dist*math.sin(radians)
         return (x,y)
 
+    # d1 = Direction 1
     def calc_diff(d1 : float, d2 : float):
         r1 = d2 - d1
         r2 = d2 - d1 - 360
         r3 = d2 - d1 + 360
         return min(r1, r2, r3, key=lambda r: abs(r))
+
+
+    def calc_angle(vector):
+        # cos(a) = abs(x) / sqrt(x^2+y^2)
+        vx, vy = vector
+        v_length = math.sqrt(math.pow(vx, 2) + math.pow(vy, 2))
+        x_length = abs(vx)
+        abs_radians = math.acos(x_length / v_length)
+        abs_degrees = math.degrees(abs_radians)
+
+        if vx < 0 and vy < 0:
+            abs_degrees += 180
+        elif vx < 0:
+            abs_degrees += 90
+        elif vy < 0:
+            abs_degrees += 270
+
+        return abs_degrees
