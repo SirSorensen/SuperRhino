cb91e45026e8705e657c4191d1596cf7ccc8af77
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2025-03-26 12:37:20 +0100

    Feat : Robot now follows tape and stops at mid on new intersections

--- pybricks/robot.py
+++ pybricks/robot.py
@@ -27,7 +27,7 @@ class Robot:
         self.compass: Compass = Compass(self.prime_hub.imu)
 
         # Initialize & awareness
-        self.spatial_awareness: Spatial_Awareness = Spatial_Awareness((2,4)) #TODO: Measure dist to eyes
+        self.spatial_awareness: Spatial_Awareness = Spatial_Awareness((2, 4))  # TODO: Measure dist to eyes
 
         # Calibrations
         self.calibrate_compass()
@@ -47,38 +47,100 @@ class Robot:
             # Go forward
             self.goto_next_intersection()
 
-
     def goto_next_intersection(self):
-        correct_angle = self.compass.direction()
+        self.tape_mid = self.follow_tape()
+        correct_angle = self.tape_mid.angle
+        self.tape_counter = 0
+        self.tape_start = None
+        self.tape_end = None
+        self.intersection_mid = None
+        self.end_point = None
 
         while True:
             self.movement.start_forward()
 
-            self.spatial_awareness.update(self.movement.distance(), self.compass.direction())
-            self.spatial_awareness.print_status()
-
+            # PID
             if self.compass.angle_needs_correcting(correct_angle):
-                self.movement.hold()
-                angle_error = self.compass.calc_error(correct_angle)
-                self.movement.turn_degrees(angle_error)
-                self.movement.start_forward()
+                self.hold()
+                self.turn_to(correct_angle)
+                self.start_forward()
 
             # I see not table!
-            if self.vision.what_is_seen() != (VisionObject.TABLE, VisionObject.TABLE):
-                self.movement.hold()
-
-                # If it is NOT an intersection, then it is probably due to being hit
-                if not self.detect_intersection():
-                    pass
-
-                # If it is an intersection, then keep going until in the middle of intersection
-                else:
-                    pass
+            self.detect_intersection()
+            if self.end_point is not None and self.spatial_awareness.cur_position.dist(self.end_point) <= 0.1:
+                self.hold()
+                return
 
 
 
-    def detect_intersection(self) -> bool:
-        pass
+    def detect_intersection(self):
+        if self.vision.what_is_seen()[0] == VisionObject.TAPE or self.vision.what_is_seen()[1] == VisionObject.TAPE:
+            if self.tape_counter == 0:
+                self.tape_start = self.get_vision(VisionObject.TAPE)
+            self.tape_counter += 1
+        else:
+            if self.get_max_tape_dist() >= 47:
+                self.tape_end = self.get_vision(VisionObject.TABLE)
+                if self.tape_start[0] is None:
+                    self.tape_end[0] = None
+                if self.tape_start[1] is None:
+                    self.tape_end[1] = None
+                self.end_point = self.calc_intersection_mid()
+
+            tape_counter = max(0, self.tape_counter - 1)
+            if tape_counter == 0:
+                self.tape_start = (None, None)
+
+    def get_vision(self, vision_object):
+        if self.vision.what_is_seen() == (vision_object, vision_object):
+            return self.spatial_awareness.get_eyes_posses()
+        elif self.vision.what_is_seen()[0] == vision_object:
+            return (self.spatial_awareness.get_eyes_posses()[0], None)
+        else:
+            return (None, self.spatial_awareness.get_eyes_posses()[1])
+
+
+    def get_max_tape_dist(self):
+        if self.tape_start == (None, None):
+            return 0
+
+        eye_poses = self.spatial_awareness.get_eyes_posses()
+
+        if self.tape_start[0] is not None and self.tape_start[1] is not None:
+            return max(self.tape_start[0].dist(eye_poses[0]), self.tape_start[1].dist(eye_poses[1]))
+        elif self.tape_start[0] is not None:
+            return self.tape_start[0].dist(eye_poses[0])
+        else:
+            return self.tape_start[1].dist(eye_poses[1])
+
+
+    def calc_intersection_mid(self):
+        left_start, right_start = self.tape_start
+        left_end, right_end = self.tape_end
+        if left_start is not None and left_end is not None:
+            left_mid = left_start.mid_point(left_end)
+        else:
+            left_mid = None
+
+        if right_start is not None and right_end is not None:
+            right_mid = right_start.mid_point(right_end)
+        else:
+            right_mid = None
+
+        if left_mid is None and right_mid is None:
+            raise ValueError("Left mid and right mid is None")
+
+        if left_mid is not None and right_mid is not None:
+            mid = left_mid.mid_point(right_mid)
+        elif left_mid is not None:
+            mid = left_mid
+        else:
+            mid = right_mid
+
+        return self.tape_mid.closest_point(mid)
+
+
+
 
     def follow_tape(self):
         start_angle = self.compass.direction()
@@ -115,7 +177,6 @@ class Robot:
         r2.Y += 100
         self.turn_to(start_angle)
 
-
         tape = Road(l1, l2, r1, r2)
         closest_point = tape.mid_fun.closest_point(self.spatial_awareness.cur_position)
 
@@ -127,10 +188,7 @@ class Robot:
         self.go_distance(d)
 
         self.turn_to(tape.mid_fun.angle)
-        return tape.mid_fun.angle
-
-
-
+        return tape.mid_fun
 
 
     ########################## utils ##########################
@@ -149,10 +207,11 @@ class Robot:
 
     def update_space(self):
         self.spatial_awareness.update(self.movement.distance(), self.compass.direction())
+        self.spatial_awareness.print_status()
 
     def turn_to(self, correct_angle):
-        correct_angle += (self.compass.heading_threshold/2)
-        error_direction : Direction = Trigonometry.get_direction(self.compass.calc_error(correct_angle))
+        correct_angle += self.compass.heading_threshold / 2
+        error_direction: Direction = Trigonometry.get_direction(self.compass.calc_error(correct_angle))
 
         while self.compass.angle_needs_correcting(correct_angle):
             self.start_turn(error_direction)
@@ -161,10 +220,10 @@ class Robot:
 
     def go_distance(self, distance):
         """
-            Drive forward a given distance
+        Drive forward a given distance
 
-            Arguments:
-            distance (int): Distance (in mm) that the robot should go forward in
+        Arguments:
+        distance (int): Distance (in mm) that the robot should go forward in
         """
         start_distance = self.movement.distance()
         while self.movement.distance() - start_distance < distance:
