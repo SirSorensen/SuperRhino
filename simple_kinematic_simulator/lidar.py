69127b7a8529ecaffb557691de0b97c5f36f21fe
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2025-04-07 11:38:51 +0200

    Feat : Lidar now has a set amount of sensors

--- simple_kinematic_simulator/lidar.py
+++ simple_kinematic_simulator/lidar.py
@@ -5,12 +5,13 @@ from sensor import SingleRayDistanceAndColorSensor
 from robot_pose import RobotPose
 
 
-beam_range = range(0, 360, 45)
+beam_range = range(15, 360, 30)
 
 
 class Lidar:
     def __init__(self, max_distance_cm = 8):
         self.sensors : dict[int, SingleRayDistanceAndColorSensor] = dict()
+        self.sensors[0] = SingleRayDistanceAndColorSensor(max_distance_cm, 0)
         for degree in beam_range:
             rad = math.radians(degree)
             s = SingleRayDistanceAndColorSensor(max_distance_cm, rad)
@@ -18,12 +19,12 @@ class Lidar:
 
 
     def generate_beam_and_measure(self, robot_pose, obstacles):
-        for degree in beam_range:
-            self.sensors[degree].generate_beam_and_measure(robot_pose, obstacles)
+        for _, sensor in self.sensors.items():
+            sensor.generate_beam_and_measure(robot_pose, obstacles)
 
     def draw(self, robot_pose : RobotPose, surface : pygame.Surface):
-        for degree in beam_range:
-            self.sensors[degree].draw(robot_pose, surface)
+        for _, sensor in self.sensors.items():
+            sensor.draw(robot_pose, surface)
 
 
 
