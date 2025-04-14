a7108ab0b090c8c2002dc58b6bd1a6b88f43eb83
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2025-04-14 11:10:08 +0200

    Refactor : Split parts of main.py into simulator and visualizer

--- simple_kinematic_simulator/main.py
+++ simple_kinematic_simulator/main.py
@@ -1,7 +1,8 @@
 import pygame
-from environment import Environment
 from pygame.locals import KEYDOWN, QUIT
-from robot import DifferentialDriveRobot
+
+from simulator import Simulator
+from visualizer import Visualizer
 
 #for potential visualization
 USE_VISUALIZATION = True
@@ -14,15 +15,11 @@ pygame.init()
 
 # Set up environment
 width, height = 1200, 800 # cm
-env = Environment(width, height)
 
-# (simulated) time taken for one cycle of the robot executing its algorithm
-robot_timestep = 0.1 # in seconds (simulated time)
+simulator = Simulator(width, height)
 
-robot = DifferentialDriveRobot(env,width/2-100,height/2-100,0)
-
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Robot Kinematic Simulator")
+if USE_VISUALIZATION:
+    visualizer = Visualizer(width, height)
 
 def main():
     global USE_VISUALIZATION, PAUSE
@@ -33,33 +30,22 @@ def main():
         for event in pygame.event.get():
             if event.type == QUIT:
                 running = False
+            
             if event.type == KEYDOWN:
                 if event.key == pygame.K_h: # use space key to toggle between visualization and headless
                     USE_VISUALIZATION = not USE_VISUALIZATION
                     print("Visualization is", "on" if USE_VISUALIZATION else "off")
                 if event.key == pygame.K_SPACE:
                     PAUSE = not PAUSE
+        
 
         if not PAUSE:
             # simulate one execution cycle of the robot
-            robot_pose = robot.move(robot_timestep)
-
+            simulator.run_tick()
 
         if USE_VISUALIZATION:
-            screen.fill((0, 0, 0))
-            # draw environment
-            env.draw(screen)
-            # draw robot
-            robot.draw(screen)
+            visualizer.visualize(simulator.env, simulator.robot)
 
-            # warn the user if collision happened
-            if robot.collided:
-                print("Collision!")
-                # Draw the animation
-                drawBoom()
-
-            pygame.display.flip()
-            pygame.display.update()
 
 
     print("total execution time:", (pygame.time.get_ticks() - start_time) / 1000, "seconds")  # runtime in seconds
@@ -67,13 +53,5 @@ def main():
     # Quit Pygame
     pygame.quit()
 
-def drawBoom():
-    font = pygame.font.SysFont("comicsansms", 172)  # pygame.font.Font(self.font, size)
-    text_surface = font.render('BOOM', True, (255, 0, 0))
-    text_rect = text_surface.get_rect(center=(width/2, height/2))
-    screen.blit(text_surface, text_rect)
-
 if __name__ == "__main__":
     main()
-
-
