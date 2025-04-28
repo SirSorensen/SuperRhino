df35c16d4e39749854b2fe2a32a8e44e78b80a22
David Martin Sørensen <75354353+SirSorensen@users.noreply.github.com>
2025-04-28 10:57:57 +0200

    Refactor : Made evolution more readable

--- simple_kinematic_simulator/evolution.py
+++ simple_kinematic_simulator/evolution.py
@@ -5,43 +5,55 @@ from Simulator.visualizer import Visualizer
 from Simulator.sim_state import SimulatorState
 
 class Evolution:
-    def __init__(self, width, height, population_size = 5, use_visualization = True, seed = 42):
-
+    def __init__(self, width, height, population_size = 5, use_visualization = False, seed = 42):
+        # Init random
         random.seed(seed)
-
+        # Init population
         self.population : list[Simulator] = []
         for _ in range(population_size):
             i = random.uniform(0, 1)
             self.population.append(Simulator(width, height, i=i))
-
+        # Configure the use of visualization
         self.use_visualization = use_visualization
-        #for potential visualization
         if self.use_visualization:
             self.visualizer = Visualizer(width, height)
 
-    def run_cycle(self):
 
+    def run_cycle(self):
         # Initialize Pygame
         pygame.init()
-
         results : list[SimulatorState] = []
 
         for sim in self.population:
-            start_time = pygame.time.get_ticks()
+            results.append(self.game_loop(sim))
 
-            # Game loop
-            while self.get_cycle_time(start_time) <= 5:
-                # simulate one execution cycle of the robot
-                sim.run_tick()
+        Evolution.print_final_result(results)
+        # Quit Pygame
+        pygame.quit()
 
-                if self.use_visualization:
-                    self.visualizer.visualize(sim.env, sim.robot)
 
-            print("\ntotal execution time:", self.get_cycle_time(start_time), "seconds")  # runtime in seconds
+    def game_loop(self, sim : Simulator):
+        start_time = pygame.time.get_ticks()
 
-            sim_state : SimulatorState = sim.get_state()
-            results.append(sim_state)
+        # Game loop
+        while self.get_cycle_time(start_time) <= 5:
+            # simulate one execution cycle of the robot
+            sim.run_tick()
 
+            if self.use_visualization:
+                self.visualizer.visualize(sim.env, sim.robot)
+
+        print("\ntotal execution time:", self.get_cycle_time(start_time), "seconds")  # runtime in seconds
+
+        sim_state : SimulatorState = sim.get_state()
+        return sim_state
+
+
+    def get_cycle_time(self, start_time):
+        return (pygame.time.get_ticks() - start_time) / 1000
+
+
+    def print_final_result(results : list[SimulatorState]):
         print("\n================\n")
         print("Final results:")
         i = 0
@@ -51,9 +63,3 @@ class Evolution:
             r.print_result()
             i += 1
         print("\n================\n")
-
-        # Quit Pygame
-        pygame.quit()
-
-    def get_cycle_time(self, start_time):
-        return (pygame.time.get_ticks() - start_time) / 1000
